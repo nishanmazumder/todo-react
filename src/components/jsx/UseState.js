eca8b5cdf9f1c401b654755e63c57b66975e0a60
Md Rashidul Azam (Nishan) <122454495+nishanEchoasoft@users.noreply.github.com>
2023-06-03 22:42:38 +0600

    form added

--- src/components/jsx/UseState.js
+++ src/components/jsx/UseState.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { sculptureList } from './data'
@@ -9,20 +10,22 @@ import { sculptureList } from './data'
 export default function UserData() {
 
    const [index, setIndex] = useState(0);
-   const [showMore, setshowMore] = useState(false)
+   const [showMore, setshowMore] = useState(false);
+   const [message, setMessage] = useState("Hi");
+   const [sentMsg, setSentMsg] = useState(false);
 
    let hasPrv = index > 0;
    let hasNxt = index < sculptureList.length
 
 
    const prvUserData = () => {
-      if(hasPrv){
+      if (hasPrv) {
          setIndex(index - 1)
       }
    }
 
    const nextUserData = () => {
-      if(hasNxt){
+      if (hasNxt) {
          setIndex(index + 1)
       }
    }
@@ -42,7 +45,7 @@ export default function UserData() {
             </Row>
             <Row>
                <Card style={{ width: '50%' }}>
-                  <Card.Img style={{width: '50%'}} variant="top" src={userData.url} alt={userData.alt} />
+                  <Card.Img style={{ width: '50%' }} variant="top" src={userData.url} alt={userData.alt} />
                   <Card.Body>
                      <Card.Title>{userData.name}</Card.Title>
                      <Card.Subtitle>{userData.artist}</Card.Subtitle>
@@ -51,7 +54,32 @@ export default function UserData() {
                   </Card.Body>
                </Card>
             </Row>
+
+            <Row>
+
+               <Form onSubmit={(e) => {
+                  e.preventDefault()
+                  setSentMsg(true)
+                  sendMessage(message)
+               }}>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                     <Form.Label>Type</Form.Label>
+                     <Form.Control type="text" placeholder="Type" value={message} onChange={e => setMessage(e.target.value)} />
+                     <Form.Text className="text-muted">
+                        {message}
+                     </Form.Text>
+                  </Form.Group>
+                  <Button variant="primary" type="submit"> Submit</Button>
+               </Form>
+
+               <h1>{sentMsg && "Thanks You!"}</h1>
+            </Row>
+
          </Container>
       </>
    )
 }
+
+function sendMessage(message) {
+   console.log(message);
+ }
