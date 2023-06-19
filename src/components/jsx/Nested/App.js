9e242fe25c74bcd8a9bdc6cccb77e77887d5ccf5
nishanmazumder <arosh019@gmail.com>
2023-06-19 22:12:13 +0600

    nested obj cloned

--- src/components/jsx/Nested/App.js
+++ src/components/jsx/Nested/App.js
@@ -4,30 +4,61 @@ import { initialTravelPlan } from './places.js';
 export default function TravelPlan() {
    const [plan, setPlan] = useState(initialTravelPlan);
 
-
-   function handleOnChange() { }
-
+   function handleOnChange(parentId, childId) {
+      const parentIds = plan[parentId];
+      const newPlaces = {
+         ...plan,
+         childIds: parentIds.childIds.filter(id => id !== childId)
+      };
+      setPlan({
+         ...plan,
+         [parentId]: newPlaces
+      });
+   }
 
    const root = plan[0];
-   const planetIds = root.childIds
+   const planetIds = root.childIds;
 
    return (
       <ol>
          {planetIds.map(id =>
-            <Place key={id} id={id} placesById={plan} onChange={handleOnChange}></Place>
+            <Place // Earth, Mars, Moon
+               key={id}
+               id={id}
+               placesById={plan}
+               parentId={0}
+               onChange={handleOnChange}>
+            </Place>
          )}
       </ol>
    )
 
 }
 
-function Place({ id, placesById, onChange }) {
+function Place({ id, placesById, parentId, onChange }) {
    const places = placesById[id]
    const placesOnPlanet = places.childIds
 
-
-
    return (
-      <li>{places.title}</li>
+      <li>{places.title}
+         <button onClick={() => {
+            onChange(parentId, id);
+         }}>
+            Complete
+         </button>
+         {placesOnPlanet.length > 0 &&
+            <ol>
+               {placesOnPlanet.map(childId =>
+                  <Place
+                     key={childId}
+                     id={childId}
+                     parentId={id}
+                     placesById={placesById}
+                     onChange={onChange}
+                  />
+               )}
+            </ol>
+         }
+      </li>
    )
 }
